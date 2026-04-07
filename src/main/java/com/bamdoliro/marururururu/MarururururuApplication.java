4ec7e829bcd81d6daef5d7ae5d053f24cb241048
leegunwoo <leegunwoo0325@gmail.com>
2026-04-07 14:31:51 +0900

    init

---
+++ src/main/java/com/bamdoliro/marururururu/MarururururuApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.marururururu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarururururuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarururururuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamdoliro/marururururu/MarururururuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.marururururu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarururururuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
